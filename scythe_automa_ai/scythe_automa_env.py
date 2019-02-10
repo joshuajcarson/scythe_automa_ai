9e5755bc4692d181481e53286af3ca4a17b638ec
Joshua Carson <joshuajcarson@gmail.com>
2019-02-10 09:38:59 -0600

    Forgot to make sure the test passed

--- scythe_automa_ai/scythe_automa_env.py
+++ scythe_automa_ai/scythe_automa_env.py
@@ -3,4 +3,4 @@ class ScytheGameStateManager():
 
     def __init__(self, automa_level='autometta'):
         self.automa_level = automa_level
-        self.player_faction = 'NA'
+        self.player_faction = 'albion'
